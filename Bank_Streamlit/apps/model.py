3ca22be90031c1c85637dfcb13ca413cd084ff00
salcao <85594595+salcao@users.noreply.github.com>
2021-07-10 21:49:46 -0400

    Updated model.py
    Updated model.py to include the 16 inputs

--- Bank_Streamlit/apps/model.py
+++ Bank_Streamlit/apps/model.py
@@ -9,44 +9,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 
 
-
-
-
 # load models
 saved_tree_clf = joblib.load('clf-best.pickle')
 
-
     
-banker1 = pd.DataFrame(
-    {   'age' : 33, 
-        'job' : 'self-employed',
-        'marital' : 'married',
-        'education' : 'secondary', 
-        'default' : 'no', 
-        'balance' : 0, 
-        'housing' : 'no',
-        'loan' : 'no',
-        'contact' : 'cellular',
-        'day' : 18, 
-        'month' : 'aug', 
-        'duration' : 73,
-        'campaign' : 7,
-        'pdays': -1, 
-        'previous' : 0,
-        'poutcome' : ['success']       
-     
-     
-    })
-
-
-
-# this is how to dynamically change text
-#prediction_state = st.markdown('calculating...')
-#saved_tree_clf.predict(banker1)
-
-
-
-
 def app():
     st.title('Model')
 
@@ -56,66 +22,85 @@ def app():
 
     # Load dataset
     df = pd.read_csv('projectdataset-1.csv')
-    st.write(df)
+    #st.write(df)
     # Model building
     st.header('Model performance')
     st.subheader('Making Prediction')
     st.markdown('**Please provide banker information**:')  # you can use markdown like this
     
     
-    # Read input 
-        '''
-    age = int(st.number_input('Age:', 0, 120, 20))
-    sex = st.selectbox('Sex', ['female', 'male'])
-    sib_sp = int(st.number_input('# of siblings / spouses aboard:', 0, 10, 0))
-    #par_ch = int(st.number_input('# of parents / children aboard:', 0, 10, 0))
-    pclass = st.selectbox('Ticket class (1 = 1st, 2 = 2nd, 3 = 3rd)', [1, 2, 3])
-    fare = int(st.number_input('# of parents / children aboard:', 0, 100, 0))
-    #embarked = st.selectbox('Port of Embarkation (C = Cherbourg, Q = Queenstown, S = Southampton)', ['C', 'Q', 'S'])
+
+
+    #inputs
+    Married = st.radio("What is your Martial Status:",("Single",'Married','Divorced'))
+    Age = st.slider('Age', min_value=18, max_value=100)
+    Education = st.selectbox('What is your Education Level:',['Primary','Secondary','Tertiary','Unknown'])
+    Job = st.selectbox('What is your Occupation:',["Admin.","Blue-collar","Entrepreneur","Housemaid","Management", "Retired","Self-employed","Services","Student","Technician","Unemployed","Unknown",])
+    Month = st.selectbox('Month', ['January','February','March','April','May','June','July','August','September','October','November','December'])
+    Duration = int(st.number_input("Enter Duration of call",00,100,00))
+    Default = st.radio("Did you default:",("Yes",'No'))
+    Loan = st.radio('Do you have a loan?', ("Yes","No"))
+    Balance = int(st.number_input('Enter an amount'))
+    Housing = st.radio("Do you have a house loan?",('Yes','No'))
+    Contact = st.radio("How were you contacted",("Cellular",'Telephone','Unknown'))
+    Day = int(st.number_input("Which day were you contacted on ",1,31,1))
+    Campaign = int(st.number_input("How many times were you contacted",0,1000,00))
+    pDays = int(st.number_input("pDays",-1,1000,00))
+    Previous = int(st.number_input("previous",-1,1000,00))
+    pOutcome = st.radio("What is the Poutcome",( "unknown","other","failure","success"))
 
     # this is how to dynamically change text
     prediction_state = st.markdown('calculating...')
 
+    banker1 = pd.DataFrame(
+        {   
+            'age' : 33, 
+            'job' : 'self-employed',
+            'marital' : 'married',
+            'education' : 'secondary', 
+            'default' : 'no', 
+            'balance' : 0, 
+            'housing' : 'no',
+            'loan' : 'no',
+            'contact' : 'cellular',
+            'day' : 18, 
+            'month' : 'aug', 
+            'duration' : 73,
+            'campaign' : 7,
+            'pdays': -1, 
+            'previous' : 0,
+            'poutcome' : ['success']   
 
 
-    st.title('Banking Decision')
-    # load dataset
-    df = pd.read_csv('projectdataset-1.csv')
-    Married = st.radio("What is your Martial Status:",
-        ("Single",'Married','Divorced'))
-    Education = st.selectbox('What is your Education Level:',
-        ["Select",'Primary','Secondary','Tertiary','Unknown'])
-    Job = st.selectbox('What is your Occupation:',
-        ["Select","Admin.","Blue-collar","Entrepreneur","Housemaid","Management", "Retired",
-        "Self-employed","Services","Student","Technician","Unemployed","Unknown",])
-    Loan = st.selectbox('Do you have a loan?', ["Select","Yes","No"])
-    #Features to possibly use for streamlit
-    # balance
-    # duration - conver seconts to mins (textbox option)
-    # campaign
-    # age
-    # day
-    Month = st.selectbox('Month', ['Select','January','February','March',
-        'April','May','June','July','August','September','October',
-        'November','December'])
-    Age = st.slider('Age', min_value=18, max_value=100)
-    Duration = st.number_input("Enter Duration of call",00,100,00)
-    st.checkbox('Check me out')
-    st.radio('Did You Default?', ['yes','no'])
-    st.button('Submit')
+            # 'job' : [Job],
+            # 'age' : [Age], 
+            # 'marital' : [Married],
+            # 'education' : [Education],
+            # 'default' : [Default], 
+            # 'balance' : [Balance], 
+            # 'housing' : [Housing],
+            # 'loan' : [Loan],
+            # 'contact' : [Contact],
+            # 'day' : [Day], 
+            # 'month' : [Month], 
+            # 'duration' : [Duration],
+            # 'campaign' : [Campaign],
+            # 'pdays': [pDays], 
+            # 'previous' : [Previous],
+            # 'poutcome' : [pOutcome]       
+         
+         
+        }
+    )
 
-    '''
-    
-    
-    
-    # Prediction :  this is how to dynamically change text
-    prediction_state = st.markdown('calculating...')
+
+
     y_pred = saved_tree_clf.predict(banker1)
 
     if y_pred[0] == 0:
-        msg = 'This passenger is predicted to be: **died**'
+        msg = 'This customer is predicted to be: **Not subscribed**'
     else:
-        msg = 'This passenger is predicted to be: **survived**'
+        msg = 'This customer is predicted to be: **subscribed**'
 
     prediction_state.markdown(msg)
     
